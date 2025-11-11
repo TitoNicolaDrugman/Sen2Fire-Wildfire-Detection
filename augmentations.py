ef369884a795fc042bbf24cd9674370e4377168d
Vlad <i.vladsn@gmail.com>
2025-11-12 00:12:53 +0800

    new augmentations

--- augmentations.py
+++ augmentations.py
@@ -44,3 +44,29 @@ class RandomVerticalFlip:
             sample['input'] = torch.flip(sample['input'], [1])
             sample['label'] = torch.flip(sample['label'], [1])
         return sample
+
+class GaussianNoise:
+    """Adds Gaussian noise to the input."""
+    def __init__(self, std=0.01, p=0.5):
+        self.std = std
+        self.p = p
+
+    def __call__(self, sample):
+        if random.random() < self.p:
+            noise = torch.randn_like(sample['input']) * self.std
+            sample['input'] = sample['input'] + noise
+        return sample
+
+class RandomBrightnessContrast:
+    """Randomly adjusts brightness and contrast."""
+    def __init__(self, brightness_limit=0.2, contrast_limit=0.2, p=0.5):
+        self.brightness_limit = brightness_limit
+        self.contrast_limit = contrast_limit
+        self.p = p
+
+    def __call__(self, sample):
+        if random.random() < self.p:
+            brightness = random.uniform(-self.brightness_limit, self.brightness_limit)
+            contrast = random.uniform(1 - self.contrast_limit, 1 + self.contrast_limit)
+            sample['input'] = sample['input'] * contrast + brightness
+        return sample
